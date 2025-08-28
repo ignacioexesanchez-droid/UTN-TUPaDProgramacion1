76bef7eca693fe1c51dca50b5508e275f97dcfae
ignacioexesanchez-droid <ignacioexesanchez@gmail.com>
2025-08-28 10:19:09 -0300

    Delete Unidad 3 directory

--- Unidad 3/complementario.py
+++
@@ -1,8 +0,0 @@
-dia= str(input("escribir dia"))
-dd = input("escribir fecha")
-mm = input("escribir mes")
-#print ( f"tu dia es { dia }  , tu fecha es { dd } y tu mes es { mm } " )
-print("dias validos son , lunes , martes , miercoles , jueves ,vieres")
-if dd < 1 or dd > 31 or mm <1 or mm >12:
- print ("error")
-    
